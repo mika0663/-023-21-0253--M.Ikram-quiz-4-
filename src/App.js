6c736b22208bc394d66ca0582e876d0b1a2383f1
mika0663 <mikrams0663@gmail.com>
2023-12-12 02:19:19 +0500

    footer and app

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/Header';
+import HeroSection from './component/HeroSection';
+import Herosection2 from './component/Herosection2';
+import Publication2 from './component/Publication2';
+import Benefits from './component/Benefits';
+import Brainstorming from './component/Brainstorming';
+import Reviews from './component/Reviews';
+import Crousal from './component/Crousal';
+import Form_section from './component/Form_section';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+    
+      <HeroSection/>
+    
+    
+      <Herosection2/>
+     
+          
+      <Reviews/>
+     
+      <Publication2/>
+     
+      <Benefits/>
+     
+      <Brainstorming/>
+     
+      <Crousal/>
+     
+      <Form_section/>
+    
+    <Footer/>
+      
+    </>
+  );
+}
+
+export default App;
